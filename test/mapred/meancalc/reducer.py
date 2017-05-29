943164cfecb9a8b09a392c95b20bf66d7aa8cffc
jonwessolomon88 <johnwessolomon88@gmail.com>
2017-05-28 23:04:01 -0500

    added unit test for reducer and made mapper unit test more concise

--- test/mapred/meancalc/reducer.py
+++ test/mapred/meancalc/reducer.py
@@ -1,23 +1,23 @@
 import unittest
 import subprocess
 
-class TestMapper(unittest.TestCase):
+class TestReducer(unittest.TestCase):
     def test_run(self):
         cmd = '''
-        echo "a\t1\nb\t2\nc\t3\na\t2" | 
-        python mapred/meancalc/mapper.py
+        echo "a\t1\nb\t2\nc\t3\na\t2" |
+        python mapred/meancalc/mapper.py |
+        sort -k1,1 |
+        python mapred/meancalc/reducer.py
         '''
         expected = [
-            ['a', '3.0', '2'],
-            ['b', '2.0', '1'],
-            ['c', '3.0', '1'],
+            ['a', '1.5'],
+            ['b', '2.0'],
+            ['c', '3.0'],
         ]
         out = subprocess.check_output(cmd, shell=True).strip()
         rows = [r.split('\t') for r in out.split('\n')]
-        rows.sort(key=lambda r:r[0])
         for i in range(len(rows)):
             self.assertEqual(rows[i], expected[i])
 
 if __name__ == '__main__':
     unittest.main()
-        
