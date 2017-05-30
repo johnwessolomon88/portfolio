745834c23a170501099839a1bfe78b8866fd48b6
Ubuntu <ubuntu@ip-172-31-9-204.us-west-2.compute.internal>
2017-05-30 06:32:18 +0000

    fixes to naive bayes reducer after discovering error in map-reduce standalone test

--- mapred/ml/naive_bayes/reducer.py
+++ mapred/ml/naive_bayes/reducer.py
@@ -1,18 +1,13 @@
 import sys
 
 if __name__ == '__main__':
-    last_ft = None
-    cur_neg = 0
-    cur_pos = 0
+    dta_on_ft = {}
     for l in sys.stdin:
         ft, neg, pos = l.strip().split('\t')
-        if last_ft == ft:
-            cur_neg += int(neg)
-            cur_pos += int(pos)
-        elif last_ft is not None:
-            print str(last_ft) + '\t' + str(cur_neg) + '\t' + str(cur_pos)
-        last_ft = ft
-        cur_neg = int(neg)
-        cur_pos = int(pos)
-    print str(last_ft) + '\t' + str(cur_neg) + '\t' + str(cur_pos)
-        
+        dta = dta_on_ft.get(ft, [0, 0])
+        dta[0] += int(neg)
+        dta[1] += int(pos)
+        dta_on_ft[ft] = dta
+
+    for ft,dta in dta_on_ft.iteritems():
+        print str(ft) + '\t' + str(dta[0]) + '\t' + str(dta[1])
